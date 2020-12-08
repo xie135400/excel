b237b907e5427819e3c17d0a2fff1aff403e1e7a
qingcheng <qingcheng@gmail.com>
2020-12-08 17:37:57 +0800

    增加CSV读写

--- excel.go
+++ excel.go
@@ -1,9 +1,12 @@
 package excel
 
 import (
+	"encoding/csv"
 	"errors"
 	"fmt"
 	"github.com/360EntSecGroup-Skylar/excelize/v2"
+	"io"
+	"os"
 	"reflect"
 	"strconv"
 	"strings"
@@ -11,10 +14,11 @@ import (
 )
 
 type Excel struct {
-	f *excelize.File
+	f     *excelize.File
 	Sheet string
 }
-func (e *Excel) ReadExcel(file string,data interface{})(err error){
+
+func (e *Excel) ReadExcel(file string, data interface{}) (err error) {
 	if e.Sheet == "" {
 		e.Sheet = "Sheet1"
 	}
@@ -22,7 +26,7 @@ func (e *Excel) ReadExcel(file string,data interface{})(err error){
 		err = errors.New("参数错误")
 		return err
 	}
-	e.f ,err = excelize.OpenFile(file)
+	e.f, err = excelize.OpenFile(file)
 	if err != nil {
 		return err
 	}
@@ -50,7 +54,7 @@ func (e *Excel) ReadExcel(file string,data interface{})(err error){
 						if excel_name == colCell {
 							map_data[i] = y
 						}
-						if enums_str := t.Field(i).Tag.Get("enums") ; enums_str != "" {
+						if enums_str := t.Field(i).Tag.Get("enums"); enums_str != "" {
 							var map_v = make(map[string]int)
 							enums_arr := strings.Split(enums_str, ",")
 							for _, v := range enums_arr {
@@ -71,45 +75,48 @@ func (e *Excel) ReadExcel(file string,data interface{})(err error){
 		if v.Type().Elem().Kind() != reflect.Ptr {
 			subv = subv.Elem()
 		}
-		v2 := reflect.Append(v,subv)
+		v2 := reflect.Append(v, subv)
 		v.Set(v2)
 		v_index_value := v.Index(v_index)
 		if v_index_value.Type().Kind() == reflect.Ptr {
 			v_index_value = v_index_value.Elem()
 		}
-		for i := 0; i < v_index_value.NumField();i++ {
+		for i := 0; i < v_index_value.NumField(); i++ {
 			if _, ok := map_data[i]; !ok {
 				continue
 			}
-			excel_value := row[map_data[i]]
+			excel_value := ""
+			if len(row) > map_data[i] {
+				excel_value = row[map_data[i]]
+			}
 			switch v_index_value.Field(i).Type().Kind() {
 			case reflect.String:
 				v_index_value.Field(i).Set(reflect.ValueOf(excel_value))
 				break
 			case reflect.Int:
-				data_v ,_ := strconv.Atoi(excel_value)
+				data_v, _ := strconv.Atoi(excel_value)
 				v_index_value.Field(i).Set(reflect.ValueOf(data_v))
 				break
 			case reflect.Int8:
-				data_v ,_ := strconv.Atoi(excel_value)
-				if enums_str := t.Field(i).Tag.Get("enums") ; enums_str != "" {
+				data_v, _ := strconv.Atoi(excel_value)
+				if enums_str := t.Field(i).Tag.Get("enums"); enums_str != "" {
 					map_v := map_arr[i]
-					if _ ,ok := map_v[excel_value] ; ok{
+					if _, ok := map_v[excel_value]; ok {
 						data_v = map_v[excel_value]
 					}
 				}
 				v_index_value.Field(i).Set(reflect.ValueOf(int8(data_v)))
 				break
 			case reflect.Int16:
-				data_v ,_ := strconv.Atoi(excel_value)
+				data_v, _ := strconv.Atoi(excel_value)
 				v_index_value.Field(i).Set(reflect.ValueOf(int16(data_v)))
 				break
 			case reflect.Int32:
-				data_v ,_ := strconv.Atoi(excel_value)
+				data_v, _ := strconv.Atoi(excel_value)
 				v_index_value.Field(i).Set(reflect.ValueOf(int32(data_v)))
 				break
 			case reflect.Int64:
-				data_v ,_ := strconv.Atoi(excel_value)
+				data_v, _ := strconv.Atoi(excel_value)
 				if t.Field(i).Tag.Get("excel_time") == "int" || t.Field(i).Tag.Get("excel_time") == "int64" {
 					local, _ := time.LoadLocation("Local")
 					t, _ := time.ParseInLocation("2006-01-02 15:04:05", excel_value, local)
@@ -118,35 +125,35 @@ func (e *Excel) ReadExcel(file string,data interface{})(err error){
 				v_index_value.Field(i).Set(reflect.ValueOf(int64(data_v)))
 				break
 			case reflect.Uint:
-				data_v ,_ := strconv.Atoi(excel_value)
+				data_v, _ := strconv.Atoi(excel_value)
 				v_index_value.Field(i).Set(reflect.ValueOf(uint(data_v)))
 				break
 			case reflect.Uint8:
-				data_v ,_ := strconv.Atoi(excel_value)
+				data_v, _ := strconv.Atoi(excel_value)
 				v_index_value.Field(i).Set(reflect.ValueOf(uint8(data_v)))
 				break
 			case reflect.Uint16:
-				data_v ,_ := strconv.Atoi(excel_value)
+				data_v, _ := strconv.Atoi(excel_value)
 				v_index_value.Field(i).Set(reflect.ValueOf(uint16(data_v)))
 				break
 			case reflect.Uint32:
-				data_v ,_ := strconv.Atoi(excel_value)
+				data_v, _ := strconv.Atoi(excel_value)
 				v_index_value.Field(i).Set(reflect.ValueOf(uint32(data_v)))
 				break
 			case reflect.Uint64:
-				data_v ,_ := strconv.Atoi(excel_value)
+				data_v, _ := strconv.Atoi(excel_value)
 				v_index_value.Field(i).Set(reflect.ValueOf(uint64(data_v)))
 				break
 			case reflect.Float32:
-				data_v,_ := strconv.ParseFloat(excel_value,64)
+				data_v, _ := strconv.ParseFloat(excel_value, 64)
 				v_index_value.Field(i).Set(reflect.ValueOf(float32(data_v)))
 				break
 			case reflect.Float64:
-				data_v,_ := strconv.ParseFloat(excel_value,64)
+				data_v, _ := strconv.ParseFloat(excel_value, 64)
 				v_index_value.Field(i).Set(reflect.ValueOf(data_v))
 				break
 			case reflect.Bool:
-				data_v,_ := strconv.ParseBool(excel_value)
+				data_v, _ := strconv.ParseBool(excel_value)
 				v_index_value.Field(i).Set(reflect.ValueOf(data_v))
 				break
 			case reflect.TypeOf(time.Time{}).Kind():
@@ -164,7 +171,173 @@ func (e *Excel) ReadExcel(file string,data interface{})(err error){
 	}
 	return
 }
-func (e *Excel) SaveExcel(file string,data interface{})(err error){
+func (e *Excel) ReadCsv(file string, data interface{}) (err error) {
+	if reflect.ValueOf(data).Kind() != reflect.Ptr || reflect.TypeOf(data).Elem().Kind() != reflect.Slice {
+		err = errors.New("参数错误")
+		return err
+	}
+	//e.f ,err = excelize.OpenFile(file)
+	//if err != nil {
+	//	return err
+	//}
+	f, err := os.Open(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	reader := csv.NewReader(f)
+	v := reflect.ValueOf(data).Elem()
+	t := reflect.TypeOf(data).Elem().Elem()
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+	// Get all the rows in the Sheet1.
+	var map_data = make(map[int]int)
+	var map_arr = make(map[int]map[string]int)
+	var v_index int = 0
+	var j int = 0
+	for {
+		row, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("Error:", err)
+			return err
+		}
+		if j == 0 {
+			for y, colCell := range row {
+				null_num := 0
+				for i := 0; i < t.NumField(); i++ {
+					excel_name := t.Field(i).Tag.Get("excel_name")
+					if excel_name == "" {
+						null_num++
+						continue
+					}
+					if excel_name == colCell {
+						map_data[i] = y
+						fmt.Println("y:", y)
+						fmt.Println("colCell:", colCell)
+						fmt.Println("[i]:", i)
+						fmt.Println("map_data[i]:", map_data[i])
+					}
+					if enums_str := t.Field(i).Tag.Get("enums"); enums_str != "" {
+						var map_v = make(map[string]int)
+						enums_arr := strings.Split(enums_str, ",")
+						for _, v := range enums_arr {
+							enums_v := strings.Split(v, ":")
+							key, _ := strconv.Atoi(enums_v[0])
+							map_v[enums_v[1]] = key
+						}
+						map_arr[i] = map_v
+					}
+				}
+
+			}
+			j++
+			continue
+		}
+		var subv reflect.Value
+		subv = reflect.New(t)
+		if v.Type().Elem().Kind() != reflect.Ptr {
+			subv = subv.Elem()
+		}
+		v2 := reflect.Append(v, subv)
+		v.Set(v2)
+		v_index_value := v.Index(v_index)
+		if v_index_value.Type().Kind() == reflect.Ptr {
+			v_index_value = v_index_value.Elem()
+		}
+		for i := 0; i < v_index_value.NumField(); i++ {
+			if _, ok := map_data[i]; !ok {
+				continue
+			}
+			excel_value := ""
+			if len(row) > map_data[i] {
+				excel_value = row[map_data[i]]
+			}
+			switch v_index_value.Field(i).Type().Kind() {
+			case reflect.String:
+				v_index_value.Field(i).Set(reflect.ValueOf(excel_value))
+				break
+			case reflect.Int:
+				data_v, _ := strconv.Atoi(excel_value)
+				v_index_value.Field(i).Set(reflect.ValueOf(data_v))
+				break
+			case reflect.Int8:
+				data_v, _ := strconv.Atoi(excel_value)
+				if enums_str := t.Field(i).Tag.Get("enums"); enums_str != "" {
+					map_v := map_arr[i]
+					if _, ok := map_v[excel_value]; ok {
+						data_v = map_v[excel_value]
+					}
+				}
+				v_index_value.Field(i).Set(reflect.ValueOf(int8(data_v)))
+				break
+			case reflect.Int16:
+				data_v, _ := strconv.Atoi(excel_value)
+				v_index_value.Field(i).Set(reflect.ValueOf(int16(data_v)))
+				break
+			case reflect.Int32:
+				data_v, _ := strconv.Atoi(excel_value)
+				v_index_value.Field(i).Set(reflect.ValueOf(int32(data_v)))
+				break
+			case reflect.Int64:
+				data_v, _ := strconv.Atoi(excel_value)
+				if t.Field(i).Tag.Get("excel_time") == "int" || t.Field(i).Tag.Get("excel_time") == "int64" {
+					local, _ := time.LoadLocation("Local")
+					t, _ := time.ParseInLocation("2006-01-02 15:04:05", excel_value, local)
+					data_v = int(t.Unix())
+				}
+				v_index_value.Field(i).Set(reflect.ValueOf(int64(data_v)))
+				break
+			case reflect.Uint:
+				data_v, _ := strconv.Atoi(excel_value)
+				v_index_value.Field(i).Set(reflect.ValueOf(uint(data_v)))
+				break
+			case reflect.Uint8:
+				data_v, _ := strconv.Atoi(excel_value)
+				v_index_value.Field(i).Set(reflect.ValueOf(uint8(data_v)))
+				break
+			case reflect.Uint16:
+				data_v, _ := strconv.Atoi(excel_value)
+				v_index_value.Field(i).Set(reflect.ValueOf(uint16(data_v)))
+				break
+			case reflect.Uint32:
+				data_v, _ := strconv.Atoi(excel_value)
+				v_index_value.Field(i).Set(reflect.ValueOf(uint32(data_v)))
+				break
+			case reflect.Uint64:
+				data_v, _ := strconv.Atoi(excel_value)
+				v_index_value.Field(i).Set(reflect.ValueOf(uint64(data_v)))
+				break
+			case reflect.Float32:
+				data_v, _ := strconv.ParseFloat(excel_value, 64)
+				v_index_value.Field(i).Set(reflect.ValueOf(float32(data_v)))
+				break
+			case reflect.Float64:
+				data_v, _ := strconv.ParseFloat(excel_value, 64)
+				v_index_value.Field(i).Set(reflect.ValueOf(data_v))
+				break
+			case reflect.Bool:
+				data_v, _ := strconv.ParseBool(excel_value)
+				v_index_value.Field(i).Set(reflect.ValueOf(data_v))
+				break
+			case reflect.TypeOf(time.Time{}).Kind():
+				local, _ := time.LoadLocation("Local")
+				t, _ := time.ParseInLocation("2006-01-02 15:04:05", excel_value, local)
+				v_index_value.Field(i).Set(reflect.ValueOf(t))
+				break
+			default:
+				//v_index_value.Field(i).Set(reflect.ValueOf(0))
+				//break
+			}
+		}
+		v_index++
+	}
+	return
+}
+
+func (e *Excel) SaveExcel(file string, data interface{}) (err error) {
 	if e.Sheet == "" {
 		e.Sheet = "Sheet1"
 	}
@@ -189,22 +362,22 @@ func (e *Excel) SaveExcel(file string,data interface{})(err error){
 		enums_str := t.Field(i).Tag.Get("enums")
 		if enums_str != "" {
 			map_data := make(map[int]string)
-			enums_arr := strings.Split(enums_str,",")
-			for _,v := range enums_arr {
-				enums_v := strings.Split(v,":")
-				key ,_ := strconv.Atoi(enums_v[0])
+			enums_arr := strings.Split(enums_str, ",")
+			for _, v := range enums_arr {
+				enums_v := strings.Split(v, ":")
+				key, _ := strconv.Atoi(enums_v[0])
 				map_data[key] = enums_v[1]
 			}
 			map_arr[i] = map_data
 		}
-		axis := fmt.Sprintf("%s1",addStr("A",int32(i-null_num)))
-		e.f.SetCellValue(e.Sheet,axis,excel_name)
+		axis := fmt.Sprintf("%s1", addStr("A", int32(i-null_num)))
+		e.f.SetCellValue(e.Sheet, axis, excel_name)
 	}
 	num := reflect.ValueOf(data).Elem().Len()
 	if num > 0 {
-		 for i := 0; i < num; i++ {
-		 	value := reflect.ValueOf(data).Elem().Index(i)
-		 	if reflect.ValueOf(data).Elem().Index(i).Type().Kind() == reflect.Ptr {
+		for i := 0; i < num; i++ {
+			value := reflect.ValueOf(data).Elem().Index(i)
+			if reflect.ValueOf(data).Elem().Index(i).Type().Kind() == reflect.Ptr {
 				value = value.Elem()
 			}
 			t := value.Type()
@@ -219,25 +392,25 @@ func (e *Excel) SaveExcel(file string,data interface{})(err error){
 				enums_str := t.Field(x).Tag.Get("enums")
 				if enums_str != "" {
 					map_data := map_arr[x]
-					if _ ,ok := map_data[int(value.Field(x).Int())] ; ok{
+					if _, ok := map_data[int(value.Field(x).Int())]; ok {
 						cell_value = reflect.ValueOf(map_data[int(value.Field(x).Int())])
 					}
 				}
 				excel_time := t.Field(x).Tag.Get("excel_time")
 				switch excel_time {
 				case "time":
-					val_str := fmt.Sprintf("%v",value.Field(x))
+					val_str := fmt.Sprintf("%v", value.Field(x))
 					cell_value = reflect.ValueOf(val_str[:19])
 					break
-				case "int","int64":
+				case "int", "int64":
 					t := time.Unix(value.Field(x).Int(), 0).Format("2006-01-02 15:04:05")
 					cell_value = reflect.ValueOf(t)
 					break
 				}
-				axis := fmt.Sprintf("%s%d",addStr("A",int32(x-null_num)),i + 2)
-				e.f.SetCellValue(e.Sheet,axis,cell_value)
+				axis := fmt.Sprintf("%s%d", addStr("A", int32(x-null_num)), i+2)
+				e.f.SetCellValue(e.Sheet, axis, cell_value)
 			}
-		 }
+		}
 	}
 	// Set value of a cell.
 	// Set active sheet of the workbook.
@@ -246,15 +419,100 @@ func (e *Excel) SaveExcel(file string,data interface{})(err error){
 	err = e.f.SaveAs(file)
 	return
 }
-func addStr(v string,add int32) string{
+
+func (e *Excel) SaveCsv(file string, data interface{}) (err error) {
+	if reflect.ValueOf(data).Kind() != reflect.Ptr || reflect.TypeOf(data).Elem().Kind() != reflect.Slice {
+		err = errors.New("参数错误")
+		return err
+	}
+	f, err := os.Create(file)
+	if err != nil {
+		fmt.Println("open file is failed, err: ", err)
+	}
+	defer f.Close()
+	// 写入UTF-8 BOM，防止中文乱码
+	f.WriteString("\xEF\xBB\xBF")
+	w := csv.NewWriter(f)
+	t := reflect.TypeOf(data).Elem().Elem()
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+	null_num := 0
+	var map_arr = make(map[int]map[int]string)
+	csvHeard := make([]string, 0)
+	for i := 0; i < t.NumField(); i++ {
+		excel_name := t.Field(i).Tag.Get("excel_name")
+		if excel_name == "" {
+			null_num++
+			continue
+		}
+		enums_str := t.Field(i).Tag.Get("enums")
+		if enums_str != "" {
+			map_data := make(map[int]string)
+			enums_arr := strings.Split(enums_str, ",")
+			for _, v := range enums_arr {
+				enums_v := strings.Split(v, ":")
+				key, _ := strconv.Atoi(enums_v[0])
+				map_data[key] = enums_v[1]
+			}
+			map_arr[i] = map_data
+		}
+		csvHeard = append(csvHeard, excel_name)
+	}
+	w.Write(csvHeard)
+	num := reflect.ValueOf(data).Elem().Len()
+	if num > 0 {
+		for i := 0; i < num; i++ {
+			value := reflect.ValueOf(data).Elem().Index(i)
+			if reflect.ValueOf(data).Elem().Index(i).Type().Kind() == reflect.Ptr {
+				value = value.Elem()
+			}
+			t := value.Type()
+			null_num := 0
+			cellData := make([]string, 0)
+			for x := 0; x < t.NumField(); x++ {
+				excel_name := t.Field(x).Tag.Get("excel_name")
+				if excel_name == "" {
+					null_num++
+					continue
+				}
+				cell_value := value.Field(x)
+				enums_str := t.Field(x).Tag.Get("enums")
+				if enums_str != "" {
+					map_data := map_arr[x]
+					if _, ok := map_data[int(value.Field(x).Int())]; ok {
+						cell_value = reflect.ValueOf(map_data[int(value.Field(x).Int())])
+					}
+				}
+				excel_time := t.Field(x).Tag.Get("excel_time")
+				switch excel_time {
+				case "time":
+					val_str := fmt.Sprintf("%v", value.Field(x))
+					cell_value = reflect.ValueOf(val_str[:19])
+					break
+				case "int", "int64":
+					t := time.Unix(value.Field(x).Int(), 0).Format("2006-01-02 15:04:05")
+					cell_value = reflect.ValueOf(t)
+					break
+				}
+				cellData = append(cellData, fmt.Sprintf("%v", cell_value))
+			}
+			w.Write(cellData)
+		}
+	}
+	w.Flush()
+	return
+}
+
+func addStr(v string, add int32) string {
 	x := []rune(v)
 	xLen := len(x)
-	if (x[xLen - 1] + add) > 90{
+	if (x[xLen-1] + add) > 90 {
 		if xLen == 1 {
-			return addStr("AA",add-1)
+			return addStr("AA", add-1)
 		}
 		if xLen == 2 {
-			return addStr(string(x[0] + 1)+"A",add - 1)
+			return addStr(string(x[0]+1)+"A", add-1)
 		}
 	}
 	x[xLen-1] = x[xLen-1] + add
--- excel_test.go
+++ excel_test.go
@@ -1,68 +1,121 @@
 package excel
 
 import (
+	"encoding/csv"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/360EntSecGroup-Skylar/excelize/v2"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
 	"testing"
 	"time"
 )
 
 type Resume struct {
-	Name string `json:"name" excel_name:"我的名字"`
-	Like string `json:"like"`
-	Sex string `json:"sex" excel_name:"我的性别"`
-	Age int `json:"age" excel_name:"我的年龄"`
-	Status int8 `json:"status" excel_name:"当前状态" enums:"0:冻结,1:开启,2:关闭"` //如设置enums 数据类型必须为 int8
-	CreateTime int64 `json:"create_time" excel_name:"创建时间" excel_time:"int"` //如设置 excel_time为int 数据类型必须为 int64
+	Name       string    `json:"name" excel_name:"我的名字"`
+	Like       string    `json:"like"`
+	Sex        string    `json:"sex" excel_name:"我的性别"`
+	Age        int       `json:"age" excel_name:"我的年龄"`
+	Status     int8      `json:"status" excel_name:"当前状态" enums:"0:冻结,1:开启,2:关闭"` //如设置enums 数据类型必须为 int8
+	CreateTime int64     `json:"create_time" excel_name:"创建时间" excel_time:"int"`  //如设置 excel_time为int 数据类型必须为 int64
 	UpdateTime time.Time `json:"update_time" excel_name:"更新时间" excel_time:"time"`
-	Test string `json:"test" excel_name:"测试1"`
-	Test2 string `json:"test2" excel_name:"测试2"`
-	Test3 string `json:"test3" excel_name:"测试3"`
-	Test4 string `json:"test4" excel_name:"测试4"`
-	Test5 string `json:"test5" excel_name:"测试5"`
-	Test6 string `json:"test6" excel_name:"测试6"`
-	Test7 string `json:"test7" excel_name:"测试7"`
-	Test8 string `json:"test8" excel_name:"测试8"`
-	Test9 string `json:"test9" excel_name:"测试9"`
-	Test15 string `json:"test15" excel_name:"测试15"`
-	Test16 string `json:"test16" excel_name:"测试16"`
-	Test17 string `json:"test17" excel_name:"测试17"`
-	Test18 string `json:"test18" excel_name:"测试18"`
-	Test19 string `json:"test19" excel_name:"测试19"`
+	Test       string    `json:"test" excel_name:"测试1"`
+	Test2      string    `json:"test2" excel_name:"测试2"`
+	Test3      string    `json:"test3" excel_name:"测试3"`
+	Test4      string    `json:"test4" excel_name:"测试4"`
+	Test5      string    `json:"test5" excel_name:"测试5"`
+	Test6      string    `json:"test6" excel_name:"测试6"`
+	Test7      string    `json:"test7" excel_name:"测试7"`
+	Test8      string    `json:"test8" excel_name:"测试8"`
+	Test9      string    `json:"test9" excel_name:"测试9"`
+	Test15     string    `json:"test15" excel_name:"测试15"`
+	Test16     string    `json:"test16" excel_name:"测试16"`
+	Test17     string    `json:"test17" excel_name:"测试17"`
+	Test18     string    `json:"test18" excel_name:"测试18"`
+	Test19     string    `json:"test19" excel_name:"测试19"`
 }
+
+type HeatMapData struct {
+	Index          string  `json:"index" excel_name:"index"`
+	BuildingsPower float64 `json:"buildings_power" excel_name:"buildings_power"`
+	Paid           float64 `json:"paid" excel_name:"paid"`
+	Power          float64 `json:"power" excel_name:"power"`
+	AllianceId     float64 `json:"alliance_id" excel_name:"alliance_id"`
+	Energy         float64 `json:"energy" excel_name:"energy"`
+	Language       float64 `json:"language" excel_name:"language"`
+	TechsPower     float64 `json:"techs_power" excel_name:"techs_power"`
+	PaidTimes      float64 `json:"paid_times" excel_name:"paid_times"`
+	BaseLevel      float64 `json:"base_level" excel_name:"base_level"`
+	Level          float64 `json:"level" excel_name:"level"`
+	BanExpire      float64 `json:"ban_expire" excel_name:"ban_expire"`
+	VipLevel       float64 `json:"vip_level" excel_name:"vip_level"`
+	Avatar         float64 `json:"avatar" excel_name:"avatar"`
+	ArmiesPower    float64 `json:"armies_power" excel_name:"armies_power"`
+	IsInternal     float64 `json:"is_internal" excel_name:"is_internal"`
+	LoginAt        float64 `json:"login_at" excel_name:"login_at"`
+	LastLoginAt    float64 `json:"last_login_at" excel_name:"last_login_at"`
+	CreatedAt      float64 `json:"created_at" excel_name:"created_at"`
+	IsInitial      float64 `json:"is_initial" excel_name:"is_initial"`
+	PaidAt         float64 `json:"paid_at" excel_name:"paid_at"`
+	UpdateAt       float64 `json:"update_at" excel_name:"update_at"`
+	LogoutAt       float64 `json:"logout_at" excel_name:"logout_at"`
+}
+
 func TestExcel(t *testing.T) {
 	var stru []Resume
 	info := Resume{
-		Name: "张三",
-		Sex:  "男",
-		Like: "game",
-		Age:  19,
+		Name:       "张三",
+		Sex:        "男",
+		Like:       "game",
+		Age:        19,
 		CreateTime: time.Now().Unix(),
 		UpdateTime: time.Now(),
-		Status: 2,
-		Test: "1",
-		Test2: "1",
-		Test3: "1",
-		Test4: "1",
-		Test5: "1",
-		Test6: "1",
-		Test7: "1",
-		Test8: "1",
-		Test9: "1",
-		Test15: "1",
-		Test16: "1",
-		Test17: "1",
-		Test18: "1",
-		Test19: "1",
+		Status:     2,
+		Test:       "1",
+		Test2:      "1",
+		Test3:      "1",
+		Test4:      "1",
+		Test5:      "1",
+		Test6:      "1",
+		Test7:      "1",
+		Test8:      "1",
+		Test9:      "1",
+		Test15:     "1",
+		Test16:     "1",
+		Test17:     "1",
+		Test18:     "1",
+		Test19:     "1",
 	}
 
-	for i := 0; i < 10;i ++ {
-		stru = append(stru,info)
+	for i := 0; i < 10; i++ {
+		stru = append(stru, info)
 	}
-	e := Excel{
-		Sheet: "Sheet1",
-	}
-	err := e.SaveExcel("test.xlsx",&stru)
+	e := Excel{}
+	err := e.SaveExcel("Book1.xlsx", &stru)
 	fmt.Println(err)
+	return
+}
+func TestCsvReadExcel(t *testing.T) {
+	file, err := os.Open("result.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		fmt.Println(record) // record has the type []string
+	}
+
 	return
 }
 func TestExcel_ReadExcel(t *testing.T) {
@@ -70,14 +123,127 @@ func TestExcel_ReadExcel(t *testing.T) {
 		Sheet: "Sheet1",
 	}
 	var stru []*Resume
-	err := e.ReadExcel("test.xlsx",&stru)
+	err := e.ReadExcel("Book1.xlsx", &stru)
 	fmt.Println(err)
-	for _,v := range stru {
+	for _, v := range stru {
 		fmt.Println(v)
 	}
+	jsonStr, _ := json.Marshal(stru)
+	fmt.Println(string(jsonStr))
 	return
 }
-func TestAddStr(t *testing.T){
 
-	fmt.Println(addStr("BZ",3))
+type chartDataRow struct {
+	X     string  `json:"x"`
+	Y     string  `json:"y"`
+	Value float64 `json:"value"`
+}
+type chartData struct {
+	Columns []string       `json:"columns"`
+	Rows    []chartDataRow `json:"rows"`
+}
+
+func TestExcel_ReadCsv(t *testing.T) {
+	e := Excel{}
+	var stru []*HeatMapData
+	err := e.ReadCsv("result.csv", &stru)
+	fmt.Println(err)
+	jsonStr, _ := json.Marshal(stru)
+	fmt.Println(string(jsonStr))
+	ChartData := chartData{}
+	ChartData.Columns = []string{"x", "y", "value"}
+	for _, v := range stru {
+		info := &chartDataRow{}
+		vStr, _ := json.Marshal(v)
+		vMap := make(map[string]interface{})
+		_ = json.Unmarshal(vStr, &vMap)
+		info.X = v.Index
+		for k, e := range vMap {
+			if k == "index" {
+				continue
+			}
+			info.Y = k
+			info.Value = e.(float64)
+			ChartData.Rows = append(ChartData.Rows, *info)
+		}
+	}
+	jsonStr, _ = json.Marshal(ChartData)
+	fmt.Println(string(jsonStr))
+	return
+}
+
+func TestRunPythonScript(t *testing.T) {
+	out, err := exec.Command("python3", "heatmap.py").Output()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	result := string(out)
+	fmt.Println("result:", result)
+	if strings.Index(result, "success") != 0 {
+		err = errors.New(fmt.Sprintf("main.py error：%s", result))
+	}
+	fmt.Println(err)
+	return
+}
+func TestAddStr(t *testing.T) {
+
+	fmt.Println(addStr("BZ", 3))
+}
+
+func Test360ReadExcel(t *testing.T) {
+	f, err := excelize.OpenFile("result.xlsx")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// Get value from cell by given worksheet name and axis.
+	cell, err := f.GetCellValue("result", "B2")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(cell)
+	// Get all the rows in the Sheet1.
+	rows, err := f.GetRows("result")
+	for _, row := range rows {
+		for _, colCell := range row {
+			fmt.Print(colCell, "\t")
+		}
+		fmt.Println()
+	}
+}
+func TestWriteCsv(t *testing.T) {
+	var stru []Resume
+	info := Resume{
+		Name:       "张三",
+		Sex:        "男",
+		Like:       "game",
+		Age:        19,
+		CreateTime: time.Now().Unix(),
+		UpdateTime: time.Now(),
+		Status:     2,
+		Test:       "1",
+		Test2:      "1",
+		Test3:      "1",
+		Test4:      "1",
+		Test5:      "1",
+		Test6:      "1",
+		Test7:      "1",
+		Test8:      "1",
+		Test9:      "1",
+		Test15:     "1",
+		Test16:     "1",
+		Test17:     "1",
+		Test18:     "1",
+		Test19:     "1",
+	}
+
+	for i := 0; i < 10; i++ {
+		stru = append(stru, info)
+	}
+	e := Excel{}
+	err := e.SaveCsv("test_02.csv", &stru)
+	fmt.Println(err)
+	return
 }
